bdc37a2d128f22827a38cc01d830c0486a0f7490
OL-YAD <olyadtemesgen@gmail.com>
2022-03-15 11:49:15 -0700

    function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,42 @@
 #include "main.h"
 /**
- * print_times_table - print the times table of an input
+ * print_times_table - Prints the times table of an input
  *
- * @n:the value of the table to be printed
+ * @n: the value of the table to be printed
  */
 void print_times_table(int n)
 {
-	int num,mult,prod;
+	int num, mult, prod;
 
-	if(n>=0 && n<=15)
+	if (n >= 0 && n <= 15)
 	{
-		for (num=0; num<=n;num++)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
 
-			for(mult=1;mult <=n;mult++)
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
-				_putchar(',');
+				_putchar(' ');
 
-				prod=num*mult;
-				if(prod<=99)
+				prod = num * mult;
+				if (prod <= 99)
 					_putchar(' ');
-				if(prod <=9)
+				if (prod <= 9)
 					_putchar(' ');
-				if(prod >= 100)
+
+				if (prod >= 100)
 				{
-					_putchar((prod/100) + '0');
-					_putchar(((prod/10)) %10 + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (prod<=99 && prod>= 10)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((prod/10) + '0');
+					_putchar((prod / 10) + '0');
 				}
-				_putchar((prod %10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
 	}
 }
-
